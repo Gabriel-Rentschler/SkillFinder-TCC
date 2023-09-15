c103fd925e1c32bf22b9fff1ed18d74d1bbec9c6
Gabriel-Rentschler <madiness444555666@gmail.com.br>
2023-09-15 15:19:56 -0300

    Criação página Home

---
+++ force-app/main/default/lwc/home/home.js
@@ -0,0 +1,9 @@
+import { LightningElement, track, wire } from 'lwc';
+import { NavigationMixin } from 'lightning/navigation'; // Certifique-se de importar corretamente
+import Everymindassets from '@salesforce/resourceUrl/Everymindassets';
+import Everymindassets2 from '@salesforce/resourceUrl/Everymindassets2';
+
+export default class Home extends NavigationMixin(LightningElement) {
+
+   
+}
